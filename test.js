4eb2b789e8682af4926bb8850deb5bc1d105db9f
milonishah9 <107255075+milonishah9@users.noreply.github.com>
2023-01-04 17:47:49 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,1 @@
-const https = require('https');
-
-https.get('https://example.com/file.csv', (response) => {
-  let data = '';
-
-  // A chunk of data has been received.
-  response.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  // The whole response has been received. Print out the result.
-  response.on('end', () => {
-    console.log(data);
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
+https://www.figma.com/proto/4pnh0RBK2MHG3vEnAM1nbq/Data-intelligence-CT-v2?page-id=0%3A1&node-id=3916%3A8214&viewport=89%2C-2866%2C0.34&scaling=contain&starting-point-node-id=790%3A1310
